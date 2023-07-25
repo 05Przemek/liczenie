cf0d4b68d5f1cf92f5edcd6525c89a9747e94d8d
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 14:54:52 +0200

    liczenie stała

---
+++ liczenie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int x = 7;
+            int y;
+            y = F(x);
+        }
+        static int F(int x)
+        {
+            int y = x + 13;
+            return y;
+        }
+    }
+}
